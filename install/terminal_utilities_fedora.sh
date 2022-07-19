d3963e35a207ce85e0edf0f9247f30205a587190
KotDimos <KotDimos@gmail.com>
2022-07-19 11:04:11 +0800

    feat: add lib

--- install/terminal_utilities_fedora.sh
+++ install/terminal_utilities_fedora.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-sudo dnf install \
-  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-# dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
 sudo dnf update
 
 # terminal utilities
@@ -13,6 +9,7 @@ sudo dnf install -y \
                     neovim \
                     mpv \
                     gcc \
+                    glibc-static \
                     git \
                     cmatrix \
                     bat \
